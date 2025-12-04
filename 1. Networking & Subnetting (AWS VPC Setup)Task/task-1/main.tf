bc5099355d5f1e5395380d04b7fa2036735fefcf
Aditya Singh Gaur <agaur4348@gmail.com>
2025-12-04 23:16:40 +0530

    Update main.tf

--- 1. Networking & Subnetting (AWS VPC Setup)Task/task-1/main.tf
+++ 1. Networking & Subnetting (AWS VPC Setup)Task/task-1/main.tf
@@ -1,1 +1,177 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# --------------------------
+# 1. VPC
+# --------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Aditya_VPC"
+  }
+}
+
+# --------------------------
+# 2. Public Subnets
+# --------------------------
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "Aditya_Public_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "Aditya_Public_Subnet_2"
+  }
+}
+
+# --------------------------
+# 3. Private Subnets
+# --------------------------
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Aditya_Private_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Aditya_Private_Subnet_2"
+  }
+}
+
+# --------------------------
+# 4. Internet Gateway
+# --------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Aditya_Internet_Gateway"
+  }
+}
+
+# --------------------------
+# 5. Elastic IP for NAT
+# --------------------------
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+# --------------------------
+# 6. NAT Gateway
+# --------------------------
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "Aditya_NAT_Gateway"
+  }
+}
+
+# --------------------------
+# 7. Route Table – Public
+# --------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Aditya_Public_RT"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_rt_a1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_b1" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# --------------------------
+# 8. Route Table – Private
+# --------------------------
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Aditya_Private_RT"
+  }
+}
+
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.natgw.id
+}
+
+resource "aws_route_table_association" "private_rt_a1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_b1" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# --------------------------
+# Outputs
+# --------------------------
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_1" {
+  value = aws_subnet.public1.id
+}
+
+output "public_subnet_2" {
+  value = aws_subnet.public2.id
+}
+
+output "private_subnet_1" {
+  value = aws_subnet.private1.id
+}
+
+output "private_subnet_2" {
+  value = aws_subnet.private2.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.natgw.id
+}
